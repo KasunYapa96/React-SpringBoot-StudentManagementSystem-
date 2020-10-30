e31affcd3f37f58f4de59afdb11c1d9d38ce1c82
KasunYapa96 <kasunishankayapa1996@gmail.com>
2020-10-30 15:07:04 +0530

    Add-Employee-REST-API

--- student-management-backend/src/main/java/com/student/management/studentmanagementweb/controller/StudentController.java
+++ student-management-backend/src/main/java/com/student/management/studentmanagementweb/controller/StudentController.java
@@ -3,10 +3,7 @@ package com.student.management.studentmanagementweb.controller;
 import com.student.management.studentmanagementweb.model.Student;
 import com.student.management.studentmanagementweb.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,8 @@ public class StudentController {
         return studentRepository.findAll();
     }
 
-
-
+    @PostMapping("/Students")
+  public  Student createStudent(@RequestBody Student student){
+        return studentRepository.save(student);
+  }
 }
